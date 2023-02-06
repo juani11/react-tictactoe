3d36fb39a851104886159e26d91db2ec71230020
juani11 <juanitagliero@gmail.com>
2023-02-06 16:20:28 -0300

    constants and logic for the game

---
+++ src/logic/general.js
@@ -0,0 +1,35 @@
+import { WIN_COMBINATIONS } from "../constants/constants";
+
+const thereIsAWinner = (board) => {
+    if (board.filter((elem) => elem !== null).length < 5) return false;
+
+    const result = WIN_COMBINATIONS.some((combination) => {
+        const [a, b, c] = combination;
+
+        if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+            console.log("Hay ganador", { combination });
+            return true;
+        }
+        return false;
+    });
+
+    return result;
+};
+
+const isBoardComplete = (board) => board.every((elem) => elem != null);
+
+const checkGameIsOver = (board) => {
+    //Check si hay ganador
+    if (thereIsAWinner(board)) {
+        return { gameIsOver: true, winner: true };
+    } else {
+        //Chek si ya se completo todo el board
+        return isBoardComplete(board)
+            ? { gameIsOver: true, winner: false }
+            : { gameIsOver: false, winner: false };
+    }
+};
+
+const isThereWinnerOrTie = (winner) => winner != null;
+
+export { thereIsAWinner, checkGameIsOver, isThereWinnerOrTie };
